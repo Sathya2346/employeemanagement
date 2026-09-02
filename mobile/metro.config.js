1e0db2beb5a01fdf659f17b242f436e90940dd22
Sathya2346 <ganesansathya2346@gmail.com>
2026-09-02 17:40:38 +0530

    Use standard Expo Metro resolution

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,14 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
-const config = getDefaultConfig(__dirname);
-const shim = path.resolve(__dirname, 'vector-icons-shim/index.js');
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === '@expo/vector-icons') {
-    return context.resolveRequest(context, shim, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
